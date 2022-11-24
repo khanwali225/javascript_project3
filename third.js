cbc722d0557fdd3230692719f42ddb187a27bf90
khanwali225 <khanwali225@yahoo.com>
2022-11-24 12:04:25 +0500

    Logic Statement

---
+++ third.js
@@ -0,0 +1,149 @@
+// ch# 4 => logic statment ( i myself take these examples from book)
+
+// if and if else statements
+
+// if => Example 1
+// let rain = true;
+// if (rain) {
+//     console.log("*** I will take my umbrella ***");
+// } else {
+//     console.log("*** I will leave my umbrella at home ***");
+// }
+
+//if => Example 2
+// let age = 14;
+// if (age < 18) {
+//     console.log("we're very sorry, but you can't get in under 18");
+// } else {
+//     console.log("Welcome!")
+// }
+
+// if => Example 3
+// let hobby = "dancing";
+// if(hobby == "coding"){
+// console.log("** I love coding too! **");
+// } else {
+// console.log("** Can you teach me that? **");
+// }
+
+            // practice exercise 4.1
+// let passExam = true;
+// if (passExam) {
+//     console.log("*** i will give treat ***")
+// } if (passExam != true) {
+//     console.log("*** No treat ***")
+// }
+
+
+// Else if 
+// let age = 12;
+// if (age < 3) {
+//     console.log("Free access")
+// } else if (age >= 3 && age < 12) {
+//     console.log("Fee is $5")
+// } else if (age >= 12 && age < 65) {
+//     console.log("Fee is $10")
+// } else if (age >= 65) {
+//     console.log("Fee is $7")
+// }
+
+            // practice exercise 4.2
+// let age = prompt("What's your age?")
+// console.log(age);
+
+// let message;
+// if (age >= 21) {
+//     message = "Allowed access to venue and allowed to buy alcohol"
+// } if (age >= 19) {
+//     message = "Allowed access to venue but denied to buy alcohol"
+// } else {
+//     message = "denied entry"
+// }
+//console.log(message);
+
+            // ternary operator
+//let age = 34
+//age < 18 ? console.log("denied"): console.log("allowed");
+
+          // practice exercise 4.3
+//piaicStudentID = true ? console.log("allowed into venue"): console.log("denied entry");
+
+          //switch statments
+
+//let studyTimeTable = prompt("What's your day routine?");
+// switch(studyTimeTable) {
+//     case "Get up":
+//         console.log("It is 6:10 AM")
+//         break;
+//     case "Breakfast":
+//         console.log("It is 6:40 PM")
+//         break;
+//     case "Newspaper":
+//         console.log("It is 7:00 PM")
+//         break;
+//     case "Watch CNN":
+//         console.log("It is 8:00 PM")
+//         break;
+//     default:
+//         console.log("Nothing")
+// }
+
+
+// let activity = prompt("What's your day routine?");
+
+// switch(activity) {
+//     case "Get up":
+//     console.log("It is 6:30AM");
+//     break;
+//     case "Breakfast":
+//     console.log("It is 7:00AM");
+//     break;
+//     case "Drive to work":
+//     console.log("It is 8:00AM");
+//     break;
+//     case "Lunch":
+//     console.log("It is 12:00PM");
+//     break;
+//     case "Drive home":
+//     console.log("It is 5:00PM");
+//     break;
+//     case "Dinner":
+//     console.log("It is 6:30PM");
+//     break;
+//     }
+
+
+    // let grade = prompt("What's your grade?")
+    // switch(grade) {
+    //     case "A+":
+    //     case "A":
+    //         console.log("Excellent job")
+    //         break;
+    //     case "B":
+    //     case "C":
+    //         console.log("You've passed")
+    //         break;
+    //     case "D":
+    //     case "C":
+    //         console.log("You've failed")
+    //         break;
+    //     default:
+    //         console.log("I dont know this grade.")
+    // }
+
+
+
+    // let grade = prompt("What's your grade?")
+    // if (grade === "A+" || grade === "A") {
+    //     console.log("Excellent job")
+    // } else if (grade === "B" || grade === "C") {
+    //     console.log("You've passed")
+    // } else if (grade ===" D " || grade === "F") {
+    //     console.log("You've failed")
+    // }
+    
+
+
+    
+
+   
